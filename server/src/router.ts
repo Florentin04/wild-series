bdbc50c8a7f2fd077d8fa77b1950d5eaf7cb35fa
Kaneb Florentin <Florentink.pro@gmail.com>
2025-06-03 15:03:50 +0200

    feat: challenge 02

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import type { RequestHandler } from "express";
 
 const router = express.Router();
 
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to wild Series !");
+};
 
+router.get("/", sayWelcome);
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
 
